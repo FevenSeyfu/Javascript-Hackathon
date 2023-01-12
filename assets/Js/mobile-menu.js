e6f70d1e88066880c873b4031490aa558f475b38
FevenSeyfu <fevenseyfu00@gmail.com>
2023-01-12 20:01:18 +0300

    Add mobile menu

---
+++ assets/Js/mobile-menu.js
@@ -0,0 +1,26 @@
+const hamburgerBtn = document.getElementById('menu-icon');
+const cancleBtn = document.getElementById('cancle-icon');
+const mobileScreen = window.matchMedia('screen and (max-width:768px)');
+const mainBody = document.getElementById('main-body');
+const mobileMenu = document.getElementById('mobile-menu');
+
+// const desktopScreen = window.matchMedia('screen and (min-width:768px)');
+
+function displayMobileMenu() {
+  if (mobileScreen.matches) {
+    // mainBody.style.display = 'none';
+    // mobileMenu.style.display = 'block';
+    mainBody.classList.replace('show', 'hide');
+    mobileMenu.classList.replace('hide', 'show');
+    mobileMenu.classList.add('mobile-menu-style');
+  }
+  // window.addEventListener('resize', reverseEvent);
+}
+
+function hideMobileMenu() {
+  mobileMenu.classList.replace('show', 'hide');
+  mainBody.classList.replace('hide', 'show');
+}
+
+hamburgerBtn.addEventListener('click', displayMobileMenu);
+cancleBtn.addEventListener('click', hideMobileMenu);
